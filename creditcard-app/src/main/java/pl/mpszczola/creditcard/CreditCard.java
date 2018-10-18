fbdc4b92f2fd9358c988aa58406b39e0ad2ab357
Maria Pszczola <mariapszczola@gmail.com>
2018-10-18 08:35:24 +0000

    i znowu...

--- creditcard-app/src/main/java/pl/mpszczola/creditcard/CreditCard.java
+++ creditcard-app/src/main/java/pl/mpszczola/creditcard/CreditCard.java
@@ -1,9 +1,10 @@
 package pl.mpszczola.creditcard;
-
+// moj komentarz
 class CreditCard {
     public void assignLimit(double money){
         
     }
+    // get limit will retur current limit
     public double getLimit(){
         return 2000;
     }
